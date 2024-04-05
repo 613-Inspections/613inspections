69cfccd293fe185f11fb2aa151ae97b4b3b77aa6
Haydar Issa <haydar.issa@hnhconsulting.ca>
2024-04-05 02:42:53 +0000

    major rework web site into react static

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,327 @@
+export function Gallery () {
+return <section id="gallery" className="section-bg">
+        <div className="container">
+          <div className="section-title" data-aos="fade-up">
+            <h2>Gallery</h2>
+            <!-- <p>
+              Magnam dolores commodi suscipit. Necessitatibus eius consequatur
+              ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
+              quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+              Quia fugiat sit in iste officiis commodi quidem hic quas.
+            </p> -->
+          </div>
+
+          <div className="row justify-content-center" data-aos="fade-up">
+            <div className="col-lg-4 col-md-6">
+              <!-- Slideshow container -->
+              <div className="slideshow-container">
+                <!-- Full-width images with number and caption text -->
+                <div className="mySlides">
+                  <div className="numbertext">1 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/1.jpeg" style="width: 100%"/>
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">2 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/2.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">3 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/3.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">4 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/4.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">5 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/5.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">6 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/6.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">7 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/7.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">8 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/8.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">9 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/9.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">10 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/10.jpeg" style="width: 100%" />
+                </div>
+
+                <!-- <div className="mySlides">
+                  <div className="numbertext">11 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/11.jpeg" style="width: 100%" />
+                </div> -->
+
+                <div className="mySlides">
+                  <div className="numbertext">12 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/12.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">13 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/13.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">14 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/14.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">15 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/15.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">16 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/16.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">17 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/17.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">18 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/18.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">19 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/19.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">20 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/20.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">21 /55</div>
+                  <img loading="lazy" async src="assets/img/gallery/21.jpeg" style="width: 100%" />
+                </div>
+
+                <!-- <div className="mySlides">
+                  <div className="numbertext">22 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/22.jpeg" style="width: 100%" />
+                </div> -->
+
+                <div className="mySlides">
+                  <div className="numbertext">23 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/23.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">24 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/24.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">25 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/25.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">26 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/26.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">27 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/27.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">28 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/28.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">29 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/29.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">30 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/30.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">31 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/55.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">32 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/3.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">33 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/33.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">34 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/34.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">35 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/35.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">36 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/36.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">37 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/37.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">38 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/38.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">39 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/39.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">40 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/40.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">41 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/41.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">42 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/42.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">43 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/43.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">44 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/44.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">45 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/45.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">46 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/46.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">47 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/47.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">48 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/48.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">49 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/49.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">50 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/50.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">51 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/51.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">52 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/52.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">53 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/53.jpeg" style="width: 100%" />
+                </div>
+                
+                <div className="mySlides">
+                  <div className="numbertext">54 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/54.jpeg" style="width: 100%" />
+                </div>
+
+                <div className="mySlides">
+                  <div className="numbertext">55 / 55</div>
+                  <img loading="lazy" async src="assets/img/gallery/55.jpeg" style="width: 100%" />
+                </div>
+                <!-- Next and previous buttons -->
+                <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a className="next" onclick="plusSlides(1)">&#10095;</a>
+              </div>
+              <br />
+
+              <!-- The dots/circles -->
+              <!-- <div style="text-align: center">
+                <span className="dot" onclick="currentSlide(1)"></span>
+                <span className="dot" onclick="currentSlide(2)"></span>
+                <span className="dot" onclick="currentSlide(3)"></span>
+                <span className="dot" onclick="currentSlide(4)"></span>
+                <span className="dot" onclick="currentSlide(5)"></span>
+                <span className="dot" onclick="currentSlide(6)"></span>
+                <span className="dot" onclick="currentSlide(7)"></span>
+                <span className="dot" onclick="currentSlide(8)"></span>
+                <span className="dot" onclick="currentSlide(9)"></span>
+                <span className="dot" onclick="currentSlide(10)"></span>
+                <span className="dot" onclick="currentSlide(11)"></span>
+                <span className="dot" onclick="currentSlide(12)"></span>
+                <span className="dot" onclick="currentSlide(13)"></span>
+                <span className="dot" onclick="currentSlide(14)"></span>
+                <span className="dot" onclick="currentSlide(15)"></span>
+                <span className="dot" onclick="currentSlide(16)"></span>
+                <span className="dot" onclick="currentSlide(17)"></span>
+                <span className="dot" onclick="currentSlide(18)"></span>
+                <span className="dot" onclick="currentSlide(19)"></span>
+                <span className="dot" onclick="currentSlide(20)"></span>
+                <span className="dot" onclick="currentSlide(21)"></span>
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </section>;
+}
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+export function HeroSection() {
+  return (
+    <section
+      id="hero"
+      className="d-flex flex-column justify-content-center align-items-center"
+    >
+      <div className="container text-center text-md-left" data-aos="fade-up">
+        <h1>
+          Know before you buy: Let our inspectors give you the full picture!
+        </h1>
+        <a href="#contact" className="btn-get-started scrollto">
+          Contact Us
+        </a>
+      </div>
+    </section>
+  );
+}

2496d8b8b9020580815feb51b8ecbb2090615902
Nathan Dyer <138619765+nathandyerr7@users.noreply.github.com>
2024-06-02 14:49:34 -0400

    Added a Google Reviews component at the bottom of the site which uses an iframe.

---
+++ src/components/GoogleReviws.tsx
@@ -0,0 +1,20 @@
+export function GoogleReviews() {
+  return (
+    <section id="reviews" className="section-bg">
+      <div className="container">
+        <div className="section-title" data-aos="fade-up">
+          <h2>Reviews</h2>
+          <p>What Our Customers Say About Us</p>
+        </div>
+        <div>
+          <iframe
+            src="https://widgets.sociablekit.com/google-reviews/iframe/25420407"
+            frameBorder="0"
+            width="100%"
+            height="1000"
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  );
+}

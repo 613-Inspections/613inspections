603061032ac633fb519b37b07c7a3edd26f28b46
Paul Al Kayal <paul.alkayal@surgilive.com>
2024-06-02 20:01:47 +0000

    Made header phone number clickable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
 import { NavBar } from "./NavBar";
 
 export function Header() {
+
+  const handlePhoneClick = () => {
+    window.location.href = "tel:613-410-1357";
+  };
+
+  
   return (
     <header id="header" className="fixed-top d-flex align-items-center">
       <div className="container d-flex justify-content-between">
@@ -8,8 +14,8 @@ export function Header() {
           <h1>
             <a href="index.html">613 Inspections Inc.</a>
           </h1>
-          <p>
-            <span className="bi bi-telephone text-white"></span> 613-410-1357
+          <p className="text-primary" onClick={handlePhoneClick}>
+            <span className="bi bi-telephone"></span> 613-410-1357
           </p>
         </div>
 
